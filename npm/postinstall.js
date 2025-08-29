12ed58bb9cca75fdd9d48eaa7b8449b9215e2788
Vedran Bilopavlović <vbilopav@gmail.com>
2025-08-29 08:43:59 +0200

    bump version v2.33.0

--- npm/postinstall.js
+++ npm/postinstall.js
@@ -6,7 +6,7 @@ const os = require("os");
 const https = require("https");
 
 const downloadDir = "../.bin/";
-const downloadFrom = "https://github.com/NpgsqlRest/NpgsqlRest/releases/download/v2.32.0-client-v2.27.1/";
+const downloadFrom = "https://github.com/NpgsqlRest/NpgsqlRest/releases/download/2.33.0/";
 
 function download(url, to, done) {
     https.get(url, (response) => {
